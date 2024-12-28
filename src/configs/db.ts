97f472ddcf4f82846ea546cd6459c44b0448522a
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2024-12-28 18:55:45 +0530

    db config file updated

--- src/configs/db.ts
+++ src/configs/db.ts
@@ -8,7 +8,7 @@ console.log("DB_PASSWORD: ", process.env.DB_PASSWORD);
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: "postgres",
-  port: 5433,
+  port: parseInt(process.env.DB_PORT),
 });
 
 export default sequelize;
