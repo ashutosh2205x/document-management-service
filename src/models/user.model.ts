08e575ee45f5f1b5992cb1aad57817be8c953a93
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2024-12-28 18:57:50 +0530

    user model config

---
+++ src/models/user.model.ts
@@ -0,0 +1,20 @@
+import sequelize from "../configs/db";
+
+const { DataTypes } = require("sequelize");
+
+export const User = sequelize.define("User", {
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    defaultValue: "viewer",
+  },
+});
