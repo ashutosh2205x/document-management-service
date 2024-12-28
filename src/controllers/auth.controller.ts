c53aabb88cced5ea41a632e023192de63e9b2234
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2024-12-28 18:56:05 +0530

    auth controller ready

---
+++ src/controllers/auth.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response, NextFunction } from "express";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { User } from "../models/user.model";
+import { catchAsync } from "../utils/catchAsync";
+import { IUserSchema } from "../utils/schema-validator/user.schema";
+import db from "../configs/db";
+
+export const signup = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password, role } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = await User.create({ email, password: hashedPassword, role: role || "viewer" });
+  console.log("newUser", newUser);
+  res.status(201).json({ email: email, message: "user created succesfully" });
+});
+
+export const login = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  console.log("body", email, password);
+  IUserSchema.parse(req.body);
+  const isUser = await User.findOne({ where: { email: email } });
+  console.log("isUser", isUser?.dataValues);
+  if (!isUser || !isUser?.dataValues) {
+    return res.status(401).json({
+      success: false,
+      message: "Incorrect email or password",
+    });
+  }
+  console.log("password match for", isUser.dataValues.password, bcrypt.compareSync(password, isUser.dataValues.password));
+  if (bcrypt.compareSync(password, isUser.dataValues.password)) {
+    let token = jwt.sign({ email }, process.env.JWT_SECRET);
+    res.json({ data: isUser.dataValues, token });
+  } else {
+    res.json({ error: "incorrect password" });
+  }
+});
