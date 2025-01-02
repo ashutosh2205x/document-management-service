57d629d03979ec05e43a9bd9e6f37f693d888b9f
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2025-01-03 02:12:36 +0530

    modified auth middleware

--- src/middlewares/auth.m.ts
+++ src/middlewares/auth.m.ts
@@ -1,6 +1,7 @@
 import { Request, Response, NextFunction } from "express";
 import { IUser } from "../types/user.type";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
+require("dotenv").config();
 
 declare global {
   namespace Express {
@@ -9,15 +10,13 @@ declare global {
     }
   }
 }
-const authenticate = (req: Request, res: Response, next: NextFunction) => {
-  const token = req.header("Authorization")?.split(" ")[1];
-  if (!token) return res.status(403).send("Access denied");
+export const authenticate = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) res.status(403).json({ error: "Unauthenticated" });
 
   jwt.verify(token, process.env.JWT_SECRET, (err: any, user: any) => {
     if (err) return res.status(403).send("Invalid token");
-    req.user = user;
+    req.user = user as any;
     next();
   });
 };
-
-export { authenticate };
