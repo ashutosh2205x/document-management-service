6fe9f4bd595aad5da3a963d0a7bbe9f3a989c68f
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2025-01-03 02:13:58 +0530

    server file configured with more security options and routes added

--- src/app.ts
+++ src/app.ts
@@ -4,18 +4,19 @@ import cors from "cors";
 import morgan from "morgan";
 import sequelize from "./configs/db";
 import xss from "xss-clean";
-import { authRoutes } from "./routes/auth.routes";
 import globalErrorHandler from "./controllers/error.controller";
 import rateLimit from "express-rate-limit";
 import helmet from "helmet";
 
+import { authRoutes } from "./routes/auth.routes";
+import { documentRoutes } from "./routes/document.routes";
+
 const app = express();
 
-// configs
 dotenv.config();
-app.use(express.json({ limit: "100kb" }));
 app.use(cors());
-app.options("*", cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 app.use(xss());
 
@@ -29,8 +30,8 @@ const limiter = rateLimit({
   message: "Too many requests, please try again in after 1 minute!",
 });
 app.use("/api", limiter);
-
 app.use("/api/auth", authRoutes);
+app.use("/api/documents", documentRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -38,6 +39,7 @@ app.get("/", (req, res) => {
 app.use(globalErrorHandler);
 
 sequelize.sync().then(async () => {
+  console.log("DB connected...");
   app.listen(process.env.PORT, () => {
     console.log(`Server running on port ${process.env.PORT}`);
   });
