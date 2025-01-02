cf0ab2b95361c205f91b56efecb060b8d0873400
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2025-01-03 02:12:01 +0530

    document model added with relations with user

---
+++ src/models/doc.model.ts
@@ -0,0 +1,27 @@
+import sequelize from "../configs/db";
+import { User } from "./user.model";
+import { DataTypes } from "sequelize";
+
+export const Document = sequelize.define("documents", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  file_name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  file_path: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+
+});
+
+Document.belongsTo(User, { foreignKey: "id" });
+User.hasMany(Document, { foreignKey: "id" });
