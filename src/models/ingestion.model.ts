a9af0bef5156cab6bf764b319c57122a13b0551f
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2025-01-04 19:57:41 +0530

    ingestion model added

---
+++ src/models/ingestion.model.ts
@@ -0,0 +1,22 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../configs/db";
+import { User } from "./user.model";
+
+export const Ingestion = sequelize.define("ingestion", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  status: {
+    type: DataTypes.ENUM("pending", "in_progress", "completed", "failed"),
+    defaultValue: "pending",
+  },
+  triggered_by: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+});
+
+Ingestion.belongsTo(User, { foreignKey: "triggered_by" });
+User.hasMany(Ingestion, { foreignKey: "triggered_by" });
