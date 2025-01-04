3a6135c1b81f74e3134ee085050ad5d8a666a355
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2025-01-04 23:53:01 +0530

    ingestion swagger documentation

--- src/routes/ingestion.routes.ts
+++ src/routes/ingestion.routes.ts
@@ -4,7 +4,49 @@ import { getIngestionStatus, triggerIngestion } from "../controllers/ingestion.c
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * tags:
+ *   name: Ingestion
+ *   description: APIs for ingestion controls
+ */
+
+
+/**
+ * @swagger
+ * /api/ingestion/trigger:
+ *   post:
+ *     summary: Trigger a new ingestion
+ *     tags: [Ingestion]
+ *     responses:
+ *       201:
+ *         description: Ingestion triggered successfully
+ */
 router.post("/trigger", [authenticate], triggerIngestion);
+
+
+
+/**
+ * @swagger
+ * /api/ingestion/{ingestionId}:
+ *   get:
+ *     summary: Get ingestion status via ingestion Id
+ *     tags: [Ingestion]
+ *     responses:
+ *       200:
+ *         description: Get ingestion status
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                  id: number,
+ *                  status: string,
+ *                  triggered_by: number,
+ *                  createdAt": string,
+ *                  updatedAt: string
+ *
+ * */
 router.get("/:ingestionId", [authenticate], getIngestionStatus);
 
 export { router as ingestionRoutes };
